d24f6ce1f52cef96f5b43bcf089d98f425303dfe
dlyons <dmlyons2@gmail.com>
2020-02-12 15:03:12 -0600

    Fixed urls to make them not hard coded.

--- tests/e2e/specs/navbar.js
+++ tests/e2e/specs/navbar.js
@@ -2,14 +2,14 @@
 
 describe("Home should have navbar", () => {
   it("Visits the app root url", () => {
-    cy.visit("localhost:8080");
+    cy.visit("./");
     cy.contains("nav", "ChemReg");
   });
 });
 
 describe("Home should have welcome msg", () => {
   it("Visits the app root url", () => {
-    cy.visit("localhost:8080");
+    cy.visit("./");
     cy.contains("h1", "Welcome to ChemReg");
   });
 });
