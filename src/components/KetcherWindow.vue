dd493b4670ffd057559aa5caff447cc2f67e9f1c
Dave Lyons <dmlyons2@gmail.com>
2020-03-13 12:12:49 -0500

    19 ketcher (#30)
    * Added ketcher component and test for placeholder
    
    * Linted :)
    
    * Test passing
    
    * Lint
    
    * Remove doubly installed Cypress
    
    The @vue/cli-plugin-e2e-cypress devDependency already includes a version
    of Cypress that it is compatible with. Cypress v4 is not yet supported.
    
    * Lint
    
    Co-authored-by: Matt Covalt <mcovalt@mailbox.org>

---
+++ src/components/KetcherWindow.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Ketcher</h2>
+    <iframe
+      id="ketcher"
+      class="ketcher"
+      data-cy="ketcher"
+      v-bind:src="ketcherURL"
+      width="800"
+      height="600"
+    >
+      ketcher
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "KetcherWindow.vue",
+  data() {
+    return {
+      ketcherURL: process.env.VUE_APP_KETCHER_URL
+    };
+  }
+};
+</script>
