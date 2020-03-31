80e98c9c35f0adbc81ba8bac1aa28f3f623e880a
Matt Covalt <mcovalt@mailbox.org>
2020-03-31 00:03:14 -0400

    Make ChemregLogo a component

---
+++ src/components/ChemregLogo.vue
@@ -0,0 +1,85 @@
+<template>
+  <svg
+    :viewBox="viewBox"
+    preserveAspectRatio="xMidYMid meet"
+    :height="size"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <filter id="shadow">
+      <feDropShadow dx="4" dy="3" stdDeviation="3" flood-opacity="0.2" />
+    </filter>
+    <g filter="url(#shadow)">
+      <g :stroke="color" stroke-width="5" fill="none">
+        <ellipse cx="50" cy="60" rx="15" ry="45">
+          <animate
+            attributeName="rx"
+            values="15; 15; 15; 30; 45; 30; 15; 0; 15; 15; 15; 15"
+            dur="10s"
+            repeatCount="indefinite"
+            v-if="animate"
+          />
+        </ellipse>
+        <ellipse cx="50" cy="60" rx="15" ry="45" transform="rotate(60,50,60)">
+          <animate
+            attributeName="rx"
+            values="15; 15; 0; 15; 30; 15; 15; 15"
+            dur="10s"
+            repeatCount="indefinite"
+            v-if="animate"
+          />
+        </ellipse>
+        <ellipse cx="50" cy="60" rx="15" ry="45" transform="rotate(120,50,60)">
+          <animate
+            attributeName="rx"
+            values="15; 15; 30; 15; 0; 15; 15; 15"
+            dur="10s"
+            repeatCount="indefinite"
+            v-if="animate"
+          />
+        </ellipse>
+      </g>
+      <circle cx="50" cy="60" r="5" :fill="color" />
+      <text
+        :fill="color"
+        dominant-baseline="central"
+        font-size="3em"
+        font-weight="lighter"
+        x="105"
+        y="60"
+      >
+        chemreg
+      </text>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "ChemregLogo",
+  props: {
+    color: {
+      type: String,
+      default: "black"
+    },
+    size: {
+      type: String,
+      default: "100%"
+    },
+    includeText: {
+      type: Boolean,
+      default: true
+    },
+    animate: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    viewBox: function() {
+      return `0 0 ${this.includeText ? "340" : "100"} 120`;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,8 @@
 <template>
-  <b-navbar toggleable="lg" type="light" variant="primary">
+  <b-navbar toggleable="lg" type="dark" variant="primary" class="shadow mb-5">
     <router-link :to="{ name: 'home' }">
       <b-navbar-brand>
-        <img :src="require(`@/assets/logo_32px.png`)" class="d-inline-block" alt />
-        ChemReg
+        <ChemregLogo color="white" size="35px" />
       </b-navbar-brand>
     </router-link>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -12,15 +11,19 @@
         <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item :to="{ name: 'login' }" data-test="login" v-if="!isLoggedIn">Login</b-nav-item>
-        <b-nav-item-dropdown name="user-profile" right v-if="isLoggedIn">
+        <b-nav-item :to="{ name: 'login' }" v-if="!isAuthenticated"
+          >Login</b-nav-item
+        >
+        <b-nav-item-dropdown name="user-profile" right v-if="username">
           <template v-slot:button-content>
-            <span class="h4">
+            <span>
               <b-icon icon="person-fill" />
-              {{ user }}
+              {{ username }}
             </span>
           </template>
-          <b-dropdown-item name="logout" @click="logout">Log Out</b-dropdown-item>
+          <b-dropdown-item name="logout" @click="logout"
+            >Log Out</b-dropdown-item
+          >
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
@@ -29,31 +32,26 @@
 
 <script>
 import { mapState, mapGetters } from "vuex";
+import ChemregLogo from "@/components/ChemregLogo";
 
 export default {
-  name: "navbar",
+  name: "NavBar",
   computed: {
-    ...mapState("auth", ["user"]),
-    ...mapGetters("auth", ["isLoggedIn"])
+    ...mapState("auth", ["username"]),
+    ...mapGetters("auth", ["isAuthenticated"])
   },
   methods: {
     logout: function() {
-      this.$store.dispatch("auth/logout").then(() => {
-        this.$router.push("/login");
-      });
+      this.$store.dispatch("auth/logout");
     }
+  },
+  components: {
+    ChemregLogo
   }
 };
 </script>
 
 <style scoped>
-.navbar {
-  font-weight: bold;
-}
-a.vue-active {
-  font-size: 1.1em;
-  text-decoration: underline;
-}
 img {
   margin-right: 5px;
 }
