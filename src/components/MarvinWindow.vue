4a75b3b73819d3efb249bcbfc70b13cc829ef763
rickD <debboutr@gmail.com>
2020-05-06 22:55:21 -0700

    #21 add marvin component and test, update template.env

---
+++ src/components/MarvinWindow.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <iframe
+      id="marvin"
+      class="marvin"
+      data-cy="marvin"
+      v-bind:src="marvinURL"
+      width="800"
+      height="600"
+      scrolling="no"
+    >
+      marvin
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MarvinWindow",
+  data() {
+    return {
+      marvinURL: process.env.VUE_APP_MARVIN_URL
+    };
+  }
+};
+</script>
