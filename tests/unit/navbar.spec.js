7acb45adc51e5dbc7779210020b9ab54f6628333
Matt Frost <mwfrost@fastmail.fm>
2020-07-14 14:00:21 -0700

    strip down navbar unit test to basics, still failing

--- tests/unit/navbar.spec.js
+++ tests/unit/navbar.spec.js
@@ -1,61 +1,17 @@
-import Vuex from "vuex";
-import { createLocalVue, mount } from "@vue/test-utils";
+import {
+  shallowMount
+} from "@vue/test-utils";
 import NavBar from "@/components/NavBar.vue";
-import auth from "../../src/store/modules/auth";
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
+const wrapper = shallowMount(NavBar)
 
-describe("NavBar.vue", () => {
-  // let actions;
-  // let state;
-  let store;
+describe('NavBar', () => {
 
-  beforeEach(() => {
-    // state = {
-    //   clicks: 2
-    // };
-
-    // actions = {
-    //   moduleActionClick: jest.fn()
-    // };
-
-    store = new Vuex.Store({
-      modules: {
-        auth: {
-          state: auth.state,
-          actions: auth.actions,
-          getters: auth.getters
-        }
-      }
-    });
+  it('has no search class by default', () => {
+    expect(wrapper.hasClass('search')).to.be.false;
   });
 
   it("provides ability to logout", () => {
     expect(typeof NavBar.methods.logout).toBe("function");
-  });
-
-  it("dispatches fetchCompound when search button is clicked", async () => {
-    const wrapper = mount(NavBar, {
-      store,
-      localVue,
-      stubs: [
-        "b-navbar",
-        "b-nav-item",
-        "b-navbar-nav",
-        "b-collapse",
-        "b-navbar-toggle",
-        "b-navbar-brand",
-        "router-link",
-        "router-link"
-      ]
-    });
-    console.log(wrapper);
-    wrapper.find("#search-button").trigger("click");
-    await wrapper.vm.$nextTick();
-
-    expect(store.dispatch).toHaveBeenCalledWith("compound/fetchCompound", {
-      msg: "Test Namespaced Dispatch"
-    });
   });
 });
