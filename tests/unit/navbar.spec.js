8e4f8c9525fc7fa4f6d027060bf1832177b56bec
Matt Frost <mwfrost@fastmail.fm>
2020-07-16 10:14:08 -0700

    WIP: back to the no-store approach

--- tests/unit/navbar.spec.js
+++ tests/unit/navbar.spec.js
@@ -4,20 +4,45 @@ import {
 import NavBar from "@/components/NavBar.vue";
 
 const wrapper = shallowMount(NavBar, {
-  mocks: {
-    $store: {
-      state: {
-        username: "alice",
-        isAuthenticated: "true"
-      }
-    }
-  },
+  stubs: [
+    "b-navbar",
+    "b-nav-item",
+    "b-navbar-nav",
+    "b-collapse",
+    "b-navbar-toggle",
+    "b-navbar-brand",
+    "router-link",
+    "router-link"
+  ],
+  computed: {
+    isAuthenticated() {
+      return true
+    },
+    username: "karyn",
+    // username: {
+    //   get: function () {
+    //     return "karyn"
+    //   },
+    //   set: function (newName) {
+    //     return yourNewName
+    //   }
+    // }
+  }
 })
+
+console.log(wrapper.html({
+  pretty: true
+}))
 
 describe('NavBar', () => {
 
+
+  it('displays a brand logo', () => {
+    expect(wrapper.find(".navbar-brand").exists()).toBe(true)
+  });
+
   it('displays a search box to an authenticated user', () => {
-    expect(wrapper.find("search").exists()).toBe(true)
+    expect(wrapper.find(".search").exists()).toBe(true)
   });
 
   it("provides ability to logout", () => {
