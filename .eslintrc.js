7a8f044e91065a45d25948dfe95eec695f3d2712
Michael Barbour <michaelbarbour163@gmail.com>
2020-08-05 12:45:15 -0400

    Add linting rules for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    "jest/globals": true
   },
   extends: [
     "plugin:vue/essential",
     "@vue/prettier",
     "plugin:cypress/recommended"
   ],
+  plugins: ["jest"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   },
   parserOptions: {
     parser: "babel-eslint"
