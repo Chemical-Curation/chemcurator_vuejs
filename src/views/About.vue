1842db871396ed411ccd325353c1f3cbaf51b04f
Matt Frost <mwfrost@fastmail.fm>
2020-08-11 14:52:55 -0700

    Fetch and display record counts

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,16 @@
   <b-row class="about">
     <b-col cols="3">
       <ChemregLogo size="100px" v-bind:animate="true" />
-      <p v-if="username" class="text-left">Welcome, {{ username }}.</p>
+      <p v-if="username"></p>
       <LoginCard v-else />
     </b-col>
     <b-col>
-      <p
-        class="lead text-justify"
-      >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p class="lead text-justify">{{ aboutBlurb }}</p>
+      <b-card-group deck>
+        <b-card title="Lists" border-variant="light">{{ stats.listCount }}</b-card>
+        <b-card title="Substances" border-variant="light">{{ stats.substanceCount }}</b-card>
+        <b-card title="Compounds" border-variant="light">{{ stats.compoundCount }}</b-card>
+      </b-card-group>
     </b-col>
     <b-col cols="1"></b-col>
   </b-row>
@@ -18,16 +21,61 @@
 import ChemregLogo from "@/components/ChemregLogo";
 import LoginCard from "@/components/LoginCard";
 import { mapState, mapGetters } from "vuex";
+import axios from "axios";
 
 export default {
   name: "About",
   computed: {
     ...mapState("auth", ["username"]),
-    ...mapGetters("auth", ["isAuthenticated"])
+    ...mapGetters("auth", ["isAuthenticated"]),
+    aboutBlurb: function() {
+      return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    }
   },
   components: {
     ChemregLogo,
     LoginCard
+  },
+  data() {
+    return {
+      stats: { substanceCount: 0, listCount: 0, compoundCount: 0 }
+    };
+  },
+  beforeMount() {
+    this.getStats();
+  },
+  methods: {
+    async getStats() {
+      await axios
+        .get("http://localhost:8099/substances")
+        .then(response => {
+          this.stats["substanceCount"] = response.data.meta.pagination.count;
+        })
+        .catch(error => {
+          this.substanceCount = "error";
+          console.log(error);
+        });
+
+      await axios
+        .get("http://localhost:8099/lists")
+        .then(response => {
+          this.stats["listCount"] = response.data.meta.pagination.count;
+        })
+        .catch(error => {
+          this.listCount = "error";
+          console.log(error);
+        });
+
+      await axios
+        .get("http://localhost:8099/compounds")
+        .then(response => {
+          this.stats["compoundCount"] = response.data.meta.pagination.count;
+        })
+        .catch(error => {
+          this.listCount = "error";
+          console.log(error);
+        });
+    }
   }
 };
 </script>
