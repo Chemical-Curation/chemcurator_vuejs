5a79962a22c1bcf95b8ef2f62327fe003ca0b7e0
Michael Barbour <michaelbarbour163@gmail.com>
2020-08-24 12:10:51 -0400

    linting

--- tests/e2e/specs/lists.js
+++ tests/e2e/specs/lists.js
@@ -1,19 +1,17 @@
-const LISTS = 
-  {
-    value: "lists",
-    response: {
-      data: [
-        {
-          attributes: {
-            label: "sample List label",
-            name: "sample List name",
-            shortDescription: "sample List short description"
-          }
+const LISTS = {
+  value: "lists",
+  response: {
+    data: [
+      {
+        attributes: {
+          label: "sample List label",
+          name: "sample List name",
+          shortDescription: "sample List short description"
         }
-      ]
-    }
-  };
-
+      }
+    ]
+  }
+};
 
 describe("The lists page", () => {
   beforeEach(() => {
@@ -26,8 +24,10 @@ describe("The lists page", () => {
     cy.contains("h1", "Lists");
   });
   it("should load all types", () => {
-      cy.get("#list-table").contains(LISTS.response.data[0].attributes.label);
-      cy.get("#list-table").contains(LISTS.response.data[0].attributes.name);
-      cy.get("#list-table").contains(LISTS.response.data[0].attributes.shortDescription);
-    });
+    cy.get("#list-table").contains(LISTS.response.data[0].attributes.label);
+    cy.get("#list-table").contains(LISTS.response.data[0].attributes.name);
+    cy.get("#list-table").contains(
+      LISTS.response.data[0].attributes.shortDescription
+    );
   });
+});
