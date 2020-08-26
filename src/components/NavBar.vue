5477d8b16652df6ce7854d5e1df37ad74bc0b4d0
Michael Barbour <michaelbarbour163@gmail.com>
2020-08-26 17:28:34 -0400

    Revert "Center search bar and rework nav with grid layout"
    This reverts commit 1526ec84fd92ea8195024ef7606b8daa92eaf376.
    Collapse wasnt working with the fix I provided.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,77 +1,64 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="primary" class="shadow mb-2 row">
-    <!-- Section 1 (Navigation) -->
-    <b-col class="d-inline-flex">
-      <router-link :to="{ name: 'home' }">
-        <b-navbar-brand>
-          <ChemregLogo color="white" size="35px" />
-        </b-navbar-brand>
-      </router-link>
-      <b-collapse is-nav>
-        <b-navbar-nav>
-          <b-nav-item
-            :to="{ name: 'controlled-vocabularies' }"
-            v-if="isAuthenticated"
-            >Vocabularies</b-nav-item
+  <b-navbar toggleable="lg" type="dark" variant="primary" class="shadow mb-2">
+    <router-link :to="{ name: 'home' }">
+      <b-navbar-brand>
+        <ChemregLogo color="white" size="35px" />
+      </b-navbar-brand>
+    </router-link>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item
+          :to="{ name: 'controlled-vocabularies' }"
+          v-if="isAuthenticated"
+          >Vocabularies</b-nav-item
+        >
+        <b-nav-item :to="{ name: 'substance' }" v-if="isAuthenticated"
+          >Substances</b-nav-item
+        >
+        <b-nav-item :to="{ name: 'lists' }" v-if="isAuthenticated"
+          >Lists</b-nav-item
+        >
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-form-input
+          class="mr-2 search"
+          v-model="searchString"
+          v-if="username"
+          placeholder="Search Compounds (cid or inchikey)"
+          data-cy="search-box"
+          @keyup.enter="searchCompound"
+        />
+        <b-button
+          class="mr-2"
+          variant="dark"
+          @click="searchCompound"
+          v-if="username"
+          :disabled="searchString.length == 0"
+          data-cy="search-button"
+        >
+          <b-icon icon="search" />
+        </b-button>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown
+          name="user-profile"
+          right
+          v-if="username"
+          data-cy="user-dropdown"
+        >
+          <template v-slot:button-content>
+            <span>
+              <b-icon icon="person-fill" />
+              {{ username }}
+            </span>
+          </template>
+          <b-dropdown-item name="logout" @click="logout" data-cy="logout-button"
+            >Log Out</b-dropdown-item
           >
-          <b-nav-item :to="{ name: 'substance' }" v-if="isAuthenticated"
-            >Substances</b-nav-item
-          >
-          <b-nav-item :to="{ name: 'lists' }" v-if="isAuthenticated"
-            >Lists</b-nav-item
-          >
-        </b-navbar-nav>
-      </b-collapse>
-    </b-col>
-
-    <!-- Section 2 (Search) -->
-    <b-col>
-      <b-collapse class="d-flex justify-content-center" is-nav>
-        <b-navbar-nav>
-          <b-form-input
-            class="mr-2 search"
-            v-model="searchString"
-            v-if="username"
-            placeholder="Search Compounds (cid or inchikey)"
-            data-cy="search-box"
-            @keyup.enter="searchCompound"
-          />
-          <b-button
-            variant="dark"
-            @click="searchCompound"
-            v-if="username"
-            :disabled="searchString.length == 0"
-            data-cy="search-button"
-          >
-            <b-icon icon="search" />
-          </b-button>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-col>
-
-    <!-- Section 3 (Profile) -->
-    <b-col>
-      <b-collapse class="d-flex justify-content-end" is-nav>
-        <b-navbar-nav>
-          <b-nav-item-dropdown
-            name="user-profile"
-            right
-            v-if="username"
-            data-cy="user-dropdown"
-          >
-            <template v-slot:button-content>
-              <span>
-                <b-icon icon="person-fill" />
-                {{ username }}
-              </span>
-            </template>
-            <b-dropdown-item name="logout" @click="logout" data-cy="logout-button"
-              >Log Out</b-dropdown-item
-            >
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-col>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </template>
 
