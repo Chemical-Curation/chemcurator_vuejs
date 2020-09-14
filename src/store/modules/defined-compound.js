1d472d3dba8a0773b3ddaa1fdc7603176e91bfe3
Michael Barbour <michaelbarbour163@gmail.com>
2020-09-14 16:58:34 -0400

    Ketcher update on change

--- src/store/modules/defined-compound.js
+++ src/store/modules/defined-compound.js
@@ -1,6 +1,7 @@
 import rootActions from "../actions.js";
 import rootMutations from "../mutations.js";
 import { substanceFormFromRelationship } from "../getters";
+import { HTTP } from "@/store/http-common";
 
 const defaultState = () => {
   return {
@@ -17,6 +18,26 @@ const actions = {
   ...rootActions,
   resourceURI: () => {
     return "definedCompounds";
+  },
+  fetchByMolfile: async ({ commit }, searchString) => {
+    await HTTP.get(
+      `/definedCompounds?include=substance&filter[molfileV2000]=${encodeURI(
+        searchString
+      )}`
+    ).then(response => {
+      const data = response.data;
+      if (data.data.length > 0) {
+        const obj = data.data.shift();
+
+        commit(`storeFetch`, {
+          attributes: obj.attributes,
+          relationships: obj.relationships
+        });
+        commit(`storeIncluded`, data.included);
+      } else {
+        commit("clearState");
+      }
+    });
   }
 };
 
