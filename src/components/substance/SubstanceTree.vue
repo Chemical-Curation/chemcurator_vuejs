fa1ee918d0629257ec89ebe7bcfaad8ce861b4ea
AScaduto <Anthony.Scaduto@gdit.com>
2020-10-14 11:48:44 -0400

    Base non-dynamic tree/sidebar component layout

---
+++ src/components/substance/SubstanceTree.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-button v-b-toggle.substance-sidebar class="btn btn-primary btn-sm"
+      >Toggle Sidebar</b-button
+    >
+    <b-sidebar id="substance-sidebar" shadow>
+      <div class="px-3 py-2"></div>
+      <b-tree-view :data="treeData"></b-tree-view>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+
+export default {
+  name: "SubstanceTree",
+  props: {
+    item: Object
+  },
+  data() {
+    return {
+      treeData: [
+        {
+          id: 0,
+          name: "attributes.updatedAt",
+          children: [
+            {
+              name: "relationships.updatedBy.data.id",
+              children: [{ name: "attributes.sid" }]
+            },
+            {
+              name: "relationships.updatedBy.data.id",
+              children: [{ name: "attributes.sid" }]
+            }
+          ]
+        }
+      ]
+    };
+  },
+  computed: {
+    ...mapGetters("auth", ["isAuthenticated"]),
+    ...mapState("substance", { substanceList: "list" })
+  },
+  mounted() {
+    this.$store.dispatch("substance/getList");
+  }
+};
+</script>
+
+<style scoped></style>
