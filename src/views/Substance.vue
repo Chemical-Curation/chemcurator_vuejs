f7ded0e595b082b449dc74be13b37bc6b420b6c0
michael-barbour <michaelbarbour163@gmail.com>
2020-11-25 14:18:09 -0500

    101 molfile stop overwrite (#126)
    * First pass
    
    * fix defined compound saving
    
    * Add massive overwrites to compound importing
    
    * Change initial file loading
    
    * Restore ketcher 'onChange' functionality
    
    * Readd Navigation blocks
    
    * Restore marvin 'onChange' functionality
    
    * Restore table loading
    
    * Update tests to reflected requested changes
    
    * Fix bug Marvin ignores first edit
    
    * Add structure loading after component is loaded for marvin and ketcher

--- src/views/Substance.vue
+++ src/views/Substance.vue
@@ -6,45 +6,16 @@
         <SubstanceForm />
       </b-col>
       <b-col>
-        <div class="row mb-3">
-          <b-form-group
-            label="Record Compound ID:"
-            label-align="left"
-            label-cols="4"
-            label-for="recordCompoundID"
-            class="col"
-          >
-            <b-form-input id="recordCompoundID" :value="cid" disabled />
-          </b-form-group>
-          <b-form-group
-            label="Structure Type:"
-            label-align="left"
-            label-cols="4"
-            label-for="compound-type-dropdown"
-            class="col"
-          >
-            <b-form-select
-              id="compound-type-dropdown"
-              v-model="type"
-              :options="options(type)"
-            >
-              <template #first>
-                <option value="none">None</option>
-                <option value="definedCompound">Defined Compound</option>
-              </template>
-            </b-form-select>
-          </b-form-group>
-        </div>
         <ChemicalEditors
-          v-show="type !== 'none'"
-          :type="type"
+          :initial-compound="compound"
           :editable="isAuthenticated"
+          @change="changed = $event"
         />
       </b-col>
     </b-row>
-    <SynonymTable :substance-id="substanceId" :editable="isAuthenticated" />
-    <SubstanceRelationshipTable class="mb-5" :substance-id="substanceId" />
-    <ListTable class="mb-5" :substance-id="substanceId" />
+    <SynonymTable :substance-id="substance.id" :editable="isAuthenticated" />
+    <SubstanceRelationshipTable class="mb-5" :substance-id="substance.id" />
+    <ListTable class="mb-5" :substance-id="substance.id" />
   </b-container>
 </template>
 
@@ -56,51 +27,35 @@ import SynonymTable from "@/components/synonyms/agSynonymTable";
 import SubstanceRelationshipTable from "@/components/substance/agSubstanceRelationshipTable";
 import ListTable from "@/components/records/agRecordTable";
 import { mapGetters, mapState } from "vuex";
+import compoundApi from "@/api/compound";
 
 export default {
   name: "home",
   data() {
     return {
-      type: "none"
+      compound: {},
+      changed: false
     };
   },
   computed: {
     ...mapGetters("auth", ["isAuthenticated"]),
-    ...mapGetters("queryStructureType", { options: "getOptions" }),
-    ...mapState("compound", { compoundType: "type" }),
-    ...mapState("compound/definedcompound", { definedCompoundData: "data" }),
-    ...mapState("compound/illdefinedcompound", {
-      illDefinedCompoundData: "data"
-    }),
-    ...mapState("queryStructureType", { qstList: "list" }),
-
-    cid: function() {
-      if (this.type === "definedCompound") return this.definedCompoundData.id;
-      else if (this.type === "none") return "";
-      return this.illDefinedCompoundData.id;
-    },
-    substanceId: function() {
-      if (
-        this.type === "definedCompound" &&
-        this.definedCompoundData.relationships?.substance
-      )
-        return this.definedCompoundData.relationships?.substance?.data?.id;
-      else if (this.illDefinedCompoundData.relationships?.substance)
-        return this.illDefinedCompoundData.relationships?.substance?.data?.id;
-      return "";
-    }
+    ...mapState("substance", { substance: "detail" }),
+    ...mapState("queryStructureType", { qstList: "list" })
   },
   watch: {
-    compoundType: function() {
-      this.type = this.compoundType;
+    substance: function() {
+      if (this.substance?.relationships.associatedCompound.data?.id)
+        this.fetchCompound(
+          this.substance?.relationships.associatedCompound.data?.id
+        );
     }
   },
   methods: {
+    fetchCompound: async function(cid) {
+      this.compound = await compoundApi.fetchCompound(cid);
+    },
     checkChanged: function(event) {
-      if (
-        this.$store.state.compound.illdefinedcompound.changed ||
-        this.$store.state.compound.definedcompound.changed
-      ) {
+      if (this.changed) {
         // below only needs to eval to a truthy value
         event.returnValue = "lose your changes?";
       }
@@ -130,10 +85,7 @@ export default {
     });
   },
   beforeRouteLeave(to, from, next) {
-    if (
-      this.$store.state.compound.illdefinedcompound.changed ||
-      this.$store.state.compound.definedcompound.changed
-    ) {
+    if (this.changed) {
       this.$bvModal
         .msgBoxConfirm(
           "Unsaved changes exist on the compound in the editor, are you okay with losing the changes?",
@@ -144,14 +96,6 @@ export default {
         )
         .then(value => {
           if (value) {
-            this.$store.dispatch(
-              "compound/definedcompound/updateChanged",
-              false
-            );
-            this.$store.dispatch(
-              "compound/illdefinedcompound/updateChanged",
-              false
-            );
             next();
           } else {
             next(false);
