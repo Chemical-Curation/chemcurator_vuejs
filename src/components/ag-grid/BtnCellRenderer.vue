862051c90b8a6fc915bc2da8c0a1cb1f2d04f7f0
Michael Barbour <michaelbarbour163@gmail.com>
2020-12-11 15:16:55 -0500

    Lint button cell renderer

--- src/components/ag-grid/BtnCellRenderer.vue
+++ src/components/ag-grid/BtnCellRenderer.vue
@@ -1,30 +1,33 @@
 <template>
   <span>
-    <b-button size="sm" variant="primary" :disabled="!hasChanged" @click="btnClickedHandler()">
+    <b-button
+      size="sm"
+      variant="primary"
+      :disabled="!hasChanged"
+      @click="btnClickedHandler()"
+    >
       Save
     </b-button>
   </span>
 </template>
 
 <script>
-  import Vue from "vue";
-  import _ from "lodash";
+import Vue from "vue";
+import _ from "lodash";
 
-  // Built using https://blog.ag-grid.com/cell-renderers-in-ag-grid-every-different-flavour/ as a guide
+// Built using https://blog.ag-grid.com/cell-renderers-in-ag-grid-every-different-flavour/ as a guide
 
-  export default Vue.extend({
-    name: "BtnCellRenderer",
-    computed: {
-      hasChanged: function() {
-        return !_.isEqual(this.params.data.data, this.params.data.initialData)
-      },
-    },
-    methods: {
-      btnClickedHandler() {
-        this.params.clicked(this.params.data);
-      }
+export default Vue.extend({
+  name: "BtnCellRenderer",
+  computed: {
+    hasChanged: function() {
+      return !_.isEqual(this.params.data.data, this.params.data.initialData);
     }
-  });
+  },
+  methods: {
+    btnClickedHandler() {
+      this.params.clicked(this.params.data);
+    }
+  }
+});
 </script>
-
-
