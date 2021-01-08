0924a3b430e3f5139340e289e88d5b25b41e1bdd
AScaduto <Anthony.Scaduto@gdit.com>
2021-01-08 11:48:06 -0500

    restoring Rick's last commit

---
+++ src/components/substance/SubstanceSearchPanel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-button v-b-toggle.substance-search-panel class="btn btn-primary btn-sm">
+      Toggle Search Results
+    </b-button>
+    <b-sidebar id="substance-search-panel" right shadow>
+      <div class="substance-search-panel-header text-center">
+        <h3 id="header-text">{{ headerText }}</h3>
+      </div>
+
+      <div class="substance-search-panel-results text-center">
+        <ul class="list-group">
+          <li class="list-group-item">Result 1</li>
+          <li class="list-group-item">Result 2</li>
+          <li class="list-group-item">Result 3</li>
+        </ul>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubstanceSearchPanel",
+  props: {
+    headerText: String
+  },
+  components: {},
+  computed: {}
+};
+</script>
+
+<style scoped></style>
