9e0b883d0a78fd567208028747f10bc87f94919b
michael-barbour <michaelbarbour163@gmail.com>
2021-01-14 12:14:00 -0500

    123 substance relationship adding (#152)
    * Refactor agGrid Tables to use a mixin
    
    * Add agGrid mixin to relationship table
    
    * Transition substance relationship table away from state to local variable management
    
    * Add basic 'forward' saving
    
    * Add custom cell renderers/editors for RelationshipType dropdown
    
    * Add relationship saving and fix initial row-add bug
    
    * rework custom-renderers.js into vue agGrid components
    
    * remove custom-renderers.js
    
    * Clean up functionality of editors
    
    * Repair test broken by new ag editor and renderer
    
    * Add tests and change table ids
    
    * update some documentation and lint
    
    * Change order of columns in substance relationship table to better match relationship semantics
    
    * Fix indeterminate failing tests
    
    Co-authored-by: Matt Frost <mwfrost@fastmail.fm>

---
+++ src/components/ag-grid/SelectObjectCellEditor.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-form-select
+    :autofocus="true"
+    :select-size="selectSize"
+    v-model="selected"
+    :options="params.values"
+    @input="onClick()"
+  />
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "SelectObjectCellRenderer",
+  data() {
+    return {
+      selectSizeMax: 5,
+      selected: null
+    };
+  },
+  computed: {
+    selectSize: function() {
+      return this.params.values.length < this.selectSizeMax
+        ? this.params.values.length
+        : this.selectSizeMax;
+    }
+  },
+  methods: {
+    getValue() {
+      return this.selected;
+    },
+
+    isPopup() {
+      return true;
+    },
+
+    onClick() {
+      this.params.api.stopEditing();
+    }
+  },
+  created() {
+    this.selected = this.params.value;
+  }
+});
+</script>
